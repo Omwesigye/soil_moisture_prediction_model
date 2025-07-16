57b8092a1d7e8928bb02f8226102b675ca2fb53d
Omwesigye <omwesigyeseezi5@gmail.com>
2025-07-16 19:04:13 +0300

    added

--- Soil_Moisture/settings.py
+++ Soil_Moisture/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'omwesigyeseezi5@gmail.com'
-EMAIL_HOST_PASSWORD = 'qybu rdif wphj gkco'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 TWILIO_ACCOUNT_SID = 'your_account_sid'
